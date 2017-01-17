454adbb8b2c67f65af95f8336866f0ad4aa84d5a
Julio B <tzoulio13@gmail.com>
2017-01-17 02:44:30 +0200

    Add SQLite support, basic db functions(insert, delete..).

--- AAEergasia3/Form1.cs
+++ AAEergasia3/Form1.cs
@@ -7,11 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace AAEergasia3 {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+            MusicLib m = new MusicLib();
+            //m.InsertNewFile("aa.mp3", "AA", "aaaa", 1111, "A#", "dottt", 111111);
+            //m.InsertNewFile("bbb.bbb", "BBBB", "werbb", 22, "CCC", "c", 2);
+            ///m.InsertNewFile("musicwow22.mp3", "asqwqw N Or", "qwe", 2006, "Thrash Metal", "english", 230);
+            //m.InsertNewFile("i333weqwe.mp3", "meee", "QWEr", 2006, "Thrash Metal", "english", 0);
+            m.InsertNewFile("memem-uouou.mp3", "ououou", "memem", 2016, "Thrash Metal", "english", 0);
+            m.InsertNewFile("Mfofo - LSSDD.mp3", "LSSDD", "Mfofoo", 2046, "Thrash Metal", "english");
+            m.InsertNewFile("HVTT.mp3", "Tor", "wqe", 20776, "Thrash Metal");
+            m.InsertNewFile("Deletethis.mp3", "Tor", "wqe", 20776, "Thrash Metal");
+            m.DeleteFile("Deletethis.mp3");
+            m.EditFileInfos("HVTT.mp3", "name", "fucccccc");
+            m.EditFileInfos("HVTT.mp3", "year", 2030);
+            SQLiteDataReader r = m.GetOrdered();
+            for (int i = 0; i < 7; i++) richTextBox1.AppendText(r.GetName(i) + " /");
+            richTextBox1.AppendText("\n\n");
+            while (r.Read()) {
+                //!richTextBox1.AppendText(r["name"]+" "+r["filename"] + "\n");
+                for(int i=0; i<7;i++) richTextBox1.AppendText(r.GetValue(i).ToString()+"/");
+                richTextBox1.AppendText("\n");
+            }
+
+
         }
     }
 }
---
+++ AAEergasia3/MusicLib.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+
+namespace AAEergasia3 {
+    class MusicLib {
+        private SQLiteConnection con;
+        private string filename = "..\\..\\Externals\\music.sqldb";
+
+        public MusicLib() {
+            if (!System.IO.File.Exists(filename)) {
+                SQLiteConnection.CreateFile(filename);
+            }
+            con = new SQLiteConnection("Data Source=" + filename + ";Version=3;");
+            con.Open();
+            string sql = "CREATE TABLE IF NOT EXISTS music (filename TEXT, name TEXT, author TEXT, year INT, genre TEXT, language TEXT, score INT, UNIQUE(filename))";
+            SQLiteCommand command = new SQLiteCommand(sql, con);
+            command.ExecuteNonQuery();
+        }
+
+        public void InsertNewFile(string filename, string name, string author, int year, string genre, string langueage="english", int score=0) {
+            string sql = "INSERT OR IGNORE INTO music (filename, name, author, year, genre, language, score) VALUES ("
+                + "'"    + filename
+                + "','"  + name
+                + "', '" + author
+                + "',"   + year
+                + ",'"   + genre
+                + "','"  + langueage
+                + "',"   + score
+                + ")";
+            SQLiteCommand command = new SQLiteCommand(sql, con);
+            command.ExecuteNonQuery();
+        }
+
+        public void DeleteFile(string filename) {
+            string sql = "DELETE FROM music WHERE filename = '" + filename + "'";
+            SQLiteCommand command = new SQLiteCommand(sql, con);
+            command.ExecuteNonQuery();
+        }
+
+        public void EditFileInfos(string filename, string column, string value) {
+            string sql = "UPDATE music SET " + column + "= '" + value + "' WHERE filename = '" + filename + "'";
+            SQLiteCommand command = new SQLiteCommand(sql, con);
+            command.ExecuteNonQuery();
+        }
+        public void EditFileInfos(string filename, string column, int value) {
+            string sql = "UPDATE music SET " + column + "= " + value + " WHERE filename = '" + filename + "'";
+            SQLiteCommand command = new SQLiteCommand(sql, con);
+            command.ExecuteNonQuery();
+        }
+
+        public SQLiteDataReader GetOrdered(string order = "score", string type = "DESC") {
+            string sql = "SELECT * FROM music ORDER BY " + order + " " + type;
+            SQLiteCommand command = new SQLiteCommand(sql, con);
+            SQLiteDataReader reader = command.ExecuteReader();
+            return reader;
+        }
+        //!!close db connection on form exit
+    }
+}

7cd76d1f1e149c9f5f827c27e5faa8a4665fdcc5
Julio B <tzoulio13@gmail.com>
2017-01-25 01:10:18 +0200

    Add main dataGridView. Wav files.

--- AAEergasia3/Form1.Designer.cs
+++ AAEergasia3/Form1.Designer.cs
@@ -23,32 +23,123 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.songsDataGridView = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.songsDataGridView)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // songsDataGridView
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(54, 78);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(493, 279);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "temp textbox.. display db rows\n.\n";
+            this.songsDataGridView.AllowUserToAddRows = false;
+            this.songsDataGridView.AllowUserToDeleteRows = false;
+            this.songsDataGridView.AllowUserToResizeColumns = false;
+            this.songsDataGridView.AllowUserToResizeRows = false;
+            this.songsDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.songsDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.songsDataGridView.BackgroundColor = System.Drawing.Color.Teal;
+            this.songsDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.songsDataGridView.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.songsDataGridView.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
+            this.songsDataGridView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.DarkSlateGray;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.songsDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.songsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.songsDataGridView.Location = new System.Drawing.Point(12, 37);
+            this.songsDataGridView.MultiSelect = false;
+            this.songsDataGridView.Name = "songsDataGridView";
+            this.songsDataGridView.ReadOnly = true;
+            this.songsDataGridView.RowHeadersVisible = false;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Teal;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+            this.songsDataGridView.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.songsDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.songsDataGridView.Size = new System.Drawing.Size(874, 480);
+            this.songsDataGridView.TabIndex = 6;
+            this.songsDataGridView.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.songsDataGridView_CellMouseDoubleClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(902, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 523);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(902, 61);
+            this.panel1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(411, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(627, 421);
-            this.Controls.Add(this.richTextBox1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(902, 584);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.songsDataGridView);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.songsDataGridView)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView songsDataGridView;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- AAEergasia3/Form1.cs
+++ AAEergasia3/Form1.cs
@@ -8,16 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SQLite;
+using System.IO;
 
 namespace AAEergasia3 {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
             MusicLib m = new MusicLib();
-            //m.InsertNewFile("aa.mp3", "AA", "aaaa", 1111, "A#", "dottt", 111111);
-            //m.InsertNewFile("bbb.bbb", "BBBB", "werbb", 22, "CCC", "c", 2);
-            ///m.InsertNewFile("musicwow22.mp3", "asqwqw N Or", "qwe", 2006, "Thrash Metal", "english", 230);
-            //m.InsertNewFile("i333weqwe.mp3", "meee", "QWEr", 2006, "Thrash Metal", "english", 0);
+            /*
+            m.InsertNewFile("aa.mp3", "AA", "aaaa", 1111, "A#", "dottt", 111111);
+            m.InsertNewFile("bbb.bbb", "BBBB", "werbb", 22, "CCC", "c", 2);
+            m.InsertNewFile("musicwow22.mp3", "asqwqw N Or", "qwe", 2006, "Thrash Metal", "english", 230);
+            m.InsertNewFile("i333weqwe.mp3", "meee", "QWEr", 2006, "Thrash Metal", "english", 0);
             m.InsertNewFile("memem-uouou.mp3", "ououou", "memem", 2016, "Thrash Metal", "english", 0);
             m.InsertNewFile("Mfofo - LSSDD.mp3", "LSSDD", "Mfofoo", 2046, "Thrash Metal", "english");
             m.InsertNewFile("HVTT.mp3", "Tor", "wqe", 20776, "Thrash Metal");
@@ -25,16 +27,47 @@ namespace AAEergasia3 {
             m.DeleteFile("Deletethis.mp3");
             m.EditFileInfos("HVTT.mp3", "name", "fucccccc");
             m.EditFileInfos("HVTT.mp3", "year", 2030);
+            */
+            /*
+            m.InsertNewFile("..\\..\\test\\Loading Loop.wav", "Loading..", "8bit", 1990, "8bit");
+            m.InsertNewFile("..\\..\\test\\Marimba Boy.wav", "Marimba", "18bit", 1990, "8bit");
+            m.InsertNewFile("..\\..\\test\\Oniku Loop.wav", "Onikk", "Ninn", 1970, "8bit");
+            */
             SQLiteDataReader r = m.GetOrdered();
-            for (int i = 0; i < 7; i++) richTextBox1.AppendText(r.GetName(i) + " /");
-            richTextBox1.AppendText("\n\n");
+            songsDataGridView.ColumnCount = 7;
+            songsDataGridView.Columns[6].Visible = false;
+            songsDataGridView.EnableHeadersVisualStyles = false;
+            for(int i =0; i<songsDataGridView.ColumnCount-1;i++) {
+                songsDataGridView.Columns[i].SortMode = DataGridViewColumnSortMode.Programmatic;
+                songsDataGridView.Columns[i].Name = r.GetName(i+1);
+            }
             while (r.Read()) {
-                //!richTextBox1.AppendText(r["name"]+" "+r["filename"] + "\n");
-                for(int i=0; i<7;i++) richTextBox1.AppendText(r.GetValue(i).ToString()+"/");
-                richTextBox1.AppendText("\n");
+                songsDataGridView.Rows.Add(new object[] { r["name"], r["author"], r["year"], r["genre"], r["language"], r["score"], r["filename"] });
             }
 
+        }
 
+        private void button1_Click(object sender, EventArgs e) {
+            /*
+            WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+            wplayer.URL = "..\\..\\SupeMario.mp3";
+            wplayer.settings.volume = 90;
+            wplayer.controls.play();
+            */
+            /*
+            System.Media.SoundPlayer mario = new System.Media.SoundPlayer(@"Super_Mario_Bros.wav");
+            mario.Play();
+            */
+
+        }
+
+        private void songsDataGridView_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e) {
+            var songFile = songsDataGridView.Rows[e.RowIndex].Cells[6].Value;
+            if (File.Exists(songFile.ToString())) {
+                label1.Text = songFile.ToString();
+                System.Media.SoundPlayer test = new System.Media.SoundPlayer(songFile.ToString());
+                test.Play();
+            } else label1.Text = "File not found";
         }
     }
 }
